4ccbb475908dc12c12c5730be302b0104f2c6935
Jude <judetochyokoye@gmail.com>
2022-07-16 04:44:54 +0100

    designed this

---
+++ script.js
@@ -0,0 +1,31 @@
+const icon = document.querySelector(".icon");
+const dropDownBg = document.querySelector(".mobile-dropdown");
+const dropDown = document.querySelector(".mobile-links");
+document.addEventListener('DOMContentLoaded', function(){
+    document.querySelector('.head').classList.remove('hide');
+    isClicked = false;
+    icon.onclick = ()=>{
+        dropDown.classList.toggle("show");
+        dropDownBg.classList.toggle("black");
+        if(!isClicked){
+            icon.classList.remove('fa-bars');
+            icon.classList.add('fa-xmark');
+            isClicked = true;
+        }else{
+            icon.classList.remove('fa-xmark');
+            icon.classList.add('fa-bars');
+            isClicked = false;
+        }
+    }
+    checkSlides();
+});
+function checkSlides(){
+    const triggerPoint = window.innerHeight/5 * 4;
+    document.querySelectorAll('.img').forEach(submain =>{
+        const boxTop = submain.getBoundingClientRect().top;
+        if(boxTop < triggerPoint){
+            submain.classList.remove('hide');
+        }
+    });
+
+}
